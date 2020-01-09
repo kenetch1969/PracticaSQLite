a7a24ef33e3dd63c37c9fb5ad709261e74bd709d
Gerardo Cruz <kenetch@hotmail.com>
2020-01-09 12:29:12 -0600

    Initial Commit

---
+++ PracticaSQLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticaSQLite
+//
+//  Created by Juan Gerardo Cruz on 1/9/20.
+//  Copyright © 2020 inventaapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
